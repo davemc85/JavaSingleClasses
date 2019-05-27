8e950103d24bb67c3ca8979f8e602e43d8e23b56
davemc85 <davemcallister@hotmail.co.uk>
2019-05-27 16:26:36 +0100

    water bottle complete

---
+++ src/test/java/WaterBottleTest.java
@@ -0,0 +1,47 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class WaterBottleTest {
+
+    WaterBottle waterBottle;
+
+    @Before
+    public void before() {
+        waterBottle = new WaterBottle(100);
+    }
+
+    @Test
+    public void hasVolume(){
+        assertEquals(100, waterBottle.getVolume());
+    }
+
+    @Test
+    public void volumeDecreasesAsDrinkTaken(){
+        waterBottle.takeDrink();
+        assertEquals(90, waterBottle.getVolume());
+    }
+
+    @Test
+    public void volumeDecreasesLotsAsMoreDrinkTaken(){
+        waterBottle.takeDrink();
+        waterBottle.takeDrink();
+        waterBottle.takeDrink();
+        waterBottle.takeDrink();
+        assertEquals(60, waterBottle.getVolume());
+    }
+
+    @Test
+    public void volumeZeroWhenItsEmpty(){
+        waterBottle.emptyBottle();
+        assertEquals(0, waterBottle.getVolume());
+    }
+
+    @Test
+    public void volumeFullWhenItsFilled(){
+        waterBottle.emptyBottle();
+        waterBottle.fillBottle();
+        assertEquals(100, waterBottle.getVolume());
+    }
+}
