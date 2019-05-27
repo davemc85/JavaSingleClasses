0011a1188b76b6637fa5f35e7ede605a38adbe6e
davemc85 <davemcallister@hotmail.co.uk>
2019-05-27 17:27:49 +0100

    printer finished

---
+++ src/test/java/PrinterTest.java
@@ -0,0 +1,44 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PrinterTest {
+
+    Printer printer;
+
+    @Before
+    public void before(){
+        printer = new Printer(250, 1000);
+    }
+
+    @Test
+    public void hasPaper(){
+        assertEquals(250, printer.getPaper());
+    }
+
+    @Test
+    public void hasToner(){
+        assertEquals(1000, printer.getToner());
+    }
+
+    @Test
+    public void testPrintPaper(){
+        printer.print(10,5);
+        assertEquals(200, printer.getPaper());
+    }
+
+    @Test
+    public void testPrintToner(){
+        printer.print(10,5);
+        assertEquals(950, printer.getToner());
+    }
+
+    @Test
+    public void testCannotPrint(){
+        printer.print(10,100);
+        assertEquals(250, printer.getPaper());
+    }
+
+
+}
